06dbcc8956aebf04add60202bd856c313b500d6e
SimonLinux <simon.egeris.svendsen@gmail.com>
2023-03-16 09:25:25 +0100

    Automatic check if windows, linux or mac

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,21 @@
 using namespace std;
 int main()
 {
-    Chess game("../Stockfish-master/src/stockfish","127.0.0.1","/dev/tty.usbserial-DK0AI63M",9600);
-    //Chess game("../stockfish/stockfish-ubuntu-20.04-x86-64","127.0.0.1","/dev/tty.usbserial-DK0AI63M",9600);
+    #if defined(__linux__) // Or #if __linux__
+        Chess game("../stockfish/stockfish-ubuntu-20.04-x86-64","127.0.0.1","/dev/tty.usbserial-DK0AI63M",9600);
+    #elif _WIN32
+        cout << "Won't work on windows" << endl;
+        return 1;
+    #else
+        Chess game("../Stockfish-master/src/stockfish", "127.0.0.1", "/dev/tty.usbserial-DK0AI63M", 9600);
+    #endif
 
-    while(1){
+    while (1)
+    {
         string move;
         cout << "Enter your move: " << endl;
-        getline(cin,move);
+        getline(cin, move);
         game.userMove(move);
         game.urMove();
-
-
-
     }
 }
